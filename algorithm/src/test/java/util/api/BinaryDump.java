419b16ce9e59f80d93ef96ee72fd0b21f6b2e450
DESKTOP-9H6ON4F\Bruce Jiang <bruces.jiang@gmail.com>
2017-06-19 22:19:23 +0800

    test Binary Dump

---
+++ algorithm/src/test/java/util/api/BinaryDump.java
@@ -0,0 +1,37 @@
+/****************************************************************
+ *  Compilation:  javac BinaryDump.java
+ *  Execution:    java BinaryDump N < file
+ *  Dependencies: BinaryStdIn.java
+ *  Data File:    http://introcs.cs.princeton.edu/stdlib/abra.txt
+ *
+ *  Read in a binary file and writes out the bits, N per line.
+ *
+ *  % more abra.txt
+ *  ABRACADABRA!
+ *
+ ***************************************************************/
+package util.api;
+
+
+/**
+ * @author  Bruce Jiang
+ */
+public class BinaryDump {
+    public static void main(String[] args){
+        int BITS_PER_LINE = 16;
+        if(args.length == 1){
+            BITS_PER_LINE = Integer.parseInt(args[0]);
+        }
+
+        int count;
+        for(count = 0; !BinaryStdIn.isEmpty(); count ++){
+            if(BITS_PER_LINE == 0){ BinaryStdIn.readBoolean(); continue;}
+            else if(count != 0 && count % BITS_PER_LINE == 0) StdOut.println();
+            if(BinaryStdIn.readBoolean()) StdOut.print(1);
+            else                          StdOut.print(0);
+        }
+
+        if(BITS_PER_LINE != 0) StdOut.println();
+        StdOut.println(count + " bits");
+    }
+}
