a1824fa4dac4c99e7d97948a40ee964eacc11d02
DESKTOP-9H6ON4F\Bruce Jiang <bruces.jiang@gmail.com>
2017-06-20 23:01:21 +0800

    Practice Question

--- exercise/src/main/java/fundamentals/basicProgModel/Ex_1_1_29.java
+++ exercise/src/main/java/fundamentals/basicProgModel/Ex_1_1_29.java
@@ -11,6 +11,23 @@ package fundamentals.basicProgModel;
  */
 public class Ex_1_1_29 {
 
+    /**
+     * Returns the {@code c} of the elements which equals {@code key} in the array {@code a}
+     * @param a the array
+     * @param key the key
+     * @return the {@code c} of elements which equals {@code key} in the array {@code a}
+     */
+    public static int count(int[] a, int key){
+        int c = 0;
+        //TODO
+        return c;
+    }
+
+    public static int rank(int[] a, int key){
+        int count = 0;
+        //TODO
+        return count;
+    }
 
     public static void main(String[] args){
 
