5759d2bc1792239e0c38a4e80bd2b38209a30068
DESKTOP-9H6ON4F\Bruce Jiang <bruces.jiang@gmail.com>
2017-06-22 20:30:14 +0800

    practice question

---
+++ exercise/src/main/java/fundamentals/basicProgModel/Ex_1_1_36.java
@@ -0,0 +1,50 @@
+package fundamentals.basicProgModel;
+
+import util.api.StdArrayIO;
+import util.api.StdRandom;
+
+/**
+ * @auther Bruce Jiang
+ */
+public class Ex_1_1_36 {
+
+
+//    /**
+//     * 随机将double数组中的元素排序
+//     * @param a
+//     */
+//    public static void shuffle(int[] a){
+//        int N = a.length; //打乱N次
+//        for(int i = 0; i < N; i ++){
+//            //将 a[i] 和 a[i..N-1]中任意一个元素交换
+//            int r = i + StdRandom.uniform(N-i);
+//            int tmp = a[r];
+//            a[r] = a[i];
+//            a[i] = tmp;
+//        }
+//    }
+    
+    public static void init(int[] a){
+        for(int i = 0; i < a.length; i ++){
+            a[i] = i;
+        }
+    }
+    public static void shuffleTest(int M, int N){
+        int[] a = new int[M];
+        int[][] c = new int[M][M];
+        for(int i = 0; i < N; i ++){
+            init(a);
+            StdRandom.shuffle(a);
+            for(int j = 0; j < M; j ++){
+                c[j][a[j]]++;
+            }
+        }
+        StdArrayIO.print(c);
+    }
+    public static void main(String[] args){
+        int M  = Integer.parseInt(args[0]);
+        int N = Integer.parseInt(args[1]);
+
+        shuffleTest(M,N);
+    }
+}
