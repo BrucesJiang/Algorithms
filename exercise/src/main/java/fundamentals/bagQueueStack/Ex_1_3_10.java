eba0bb25bc0db7275700eb5136356fd50b6909c3
DESKTOP-9H6ON4F\Bruce Jiang <bruces.jiang@gmail.com>
2017-07-01 11:09:31 +0800

    Practice Question

--- exercise/src/main/java/fundamentals/bagQueueStack/Ex_1_3_10.java
+++ exercise/src/main/java/fundamentals/bagQueueStack/Ex_1_3_10.java
@@ -12,27 +12,41 @@ package fundamentals.bagQueueStack;
 
 import fundamentals.bagsQueueStack.api.Stack;
 import util.api.StdIn;
+import util.api.StdOut;
 
 /**
  * @auther Bruce Jiang
  */
 public class Ex_1_3_10 {
 
-    public static void infixToPostfix(String[] strs){
-        Stack<String> stack = new Stack<String>();
-
+    public static String infixToPostfix(String[] strs){
+        Stack<String> vals = new Stack<String>();
+        Stack<String> opts = new Stack<String>();
         int len = strs.length;
 
         for(int i = 0; i < len; i ++){
-            if(strs[i].equals("+") || strs[i].equals("-") || strs[i].equals("/") || strs[i].equals("*")){
-
+            if(strs[i].equals("+") || strs[i].equals("-") || strs[i].equals("/") || strs[i].equals("*") || strs[i].equals("sqrt")){
+                opts.push(strs[i]);
+            }else if(strs[i].equals("(")){
+                continue;
+            }else if(strs[i].equals(")")){
+                String opt = opts.pop();
+                String val = vals.pop();
+                if(opt.equals("+") || opt.equals("-") || opt.equals("/") || opt.equals("*")){
+                    vals.push(String.format("%s%s%s", vals.pop(), val, opt));
+                }else if(opt.equals("sqrt")){
+                    vals.push(String.format("%s%s", val, opt));
+                }
+            }else{
+                vals.push(strs[i]);
             }
         }
+        return vals.toString();
     }
 
     public static void main(String[] args){
         String[] strs = StdIn.readAllStrings();
-        infixToPostfix(strs);
+        StdOut.println(infixToPostfix(strs));
     }
 
 }
