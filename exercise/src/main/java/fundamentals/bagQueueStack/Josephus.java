61a58745ea9c36bb1a2f744691f0991587bf3b7e
DESKTOP-HRNL938\Bruce Jiang <1948871867@qq.com>
2017-07-03 00:20:48 +0800

    Practice Question: Josephus

--- exercise/src/main/java/fundamentals/bagQueueStack/Josephus.java
+++ exercise/src/main/java/fundamentals/bagQueueStack/Josephus.java
@@ -13,6 +13,9 @@ package fundamentals.bagQueueStack;
 import fundamentals.bagsQueueStack.api.Queue;
 import util.api.StdOut;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 /**
  * @auther Bruce Jiang
  */
@@ -20,7 +23,26 @@ public class Josephus {
     public static void main(String[] args){
         int N = Integer.parseInt(args[0]);
         int step = Integer.parseInt(args[0]);
+        josephusList(N, step);
+        StdOut.println();
+        josephusQueue(N, step);
+    }
 
+    public static void josephusList(int N, int step){
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i = 1; i <= N; i ++){
+            list.add(i);
+        }
+        while(list.size() != 0){
+            for(int i = 0; i < step; i ++){
+                int cur = list.remove(0);
+                list.add(cur);
+            }
+            StdOut.print(list.remove(0) + " ");
+        }
+    }
+
+    public static void josephusQueue(int N, int step){
         Queue<Integer> queue = new Queue<Integer>();
         for(int i = 1; i <= N; i ++){
             queue.enqueue(i);
