3be07ca97d2e645096f96385d666ec9b815f7204
DESKTOP-9H6ON4F\Bruce Jiang <bruces.jiang@gmail.com>
2017-07-15 22:09:11 +0800

    Practice Quesiton

--- exercise/src/main/java/sorting/elementarySorting/Ex_2_1_31.java
+++ exercise/src/main/java/sorting/elementarySorting/Ex_2_1_31.java
@@ -1,5 +1,8 @@
 package sorting.elementarySorting;
 
+import fundamentals.analysisOfAlgorithm.Stopwatch;
+import util.api.StdRandom;
+
 /**
  * @auther Bruce Jiang
  */
@@ -7,6 +10,24 @@ public class Ex_2_1_31 {
 
 
     public static void main(String[] args){
+        int M = Integer.parseInt(args[1]); // 问题规模的数量
+        int[][] num = new int[M][];
+        double[] timer = new double[M];
+        for(int i = 0; i < M; i ++){
+            int N = (int)StdRandom.uniform(); // 生成一个规模为N的问题
+            num[i] = new int[N];
+            for(int j =0 ; j < N; j ++){
+                num[i][j] = StdRandom.uniform(N);
+            }
+        }
+    }
+
+    public static double[][] timer(int[][] nums){
+        return null;
+    }
+
+
+    public static void draw(int[] num, double[] time){
 
     }
 }
